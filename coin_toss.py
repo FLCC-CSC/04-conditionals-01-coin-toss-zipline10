a828180b662966bcbffcc9f3ecf108e32605ce0e
johnghidiuflcc <john.ghidiu@flcc.edu>
2025-09-29 23:39:30 -0400

    Create coin_toss.py

---
+++ coin_toss.py
@@ -0,0 +1,67 @@
+# FILE NAME - coin_toss.py
+# NAME: 
+# DATE: 
+# BRIEF DESCRIPTION:  
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions and Attestation below
+# 4. The Sample Output has been included in this code for your convenience
+########## ENTER YER CODE BELOW THIS LINE ##########
+
+
+
+
+
+
+
+
+
+
+########### END YER CODE ABOVE THIS LINE ###########
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+'''
+===== Coin Flipper =====
+Heads
+'''
+
+
+
+'''
+===== Coin Flipper =====
+Tails
+'''
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. What was the hardest part of completing this lab? 
+
+
+
+
+
+
+
+'''
+
+########################################
+#            ATTESTATION
+########################################
+'''
+It is critical in this class that you understand the concepts as we explore them because
+those concepts are required understanding for entry level programming. Reliance on resources
+like AI and internet sites like Chegg, CourseHero, StackOverflow, and general Google results
+may impede your understanding. Please rate how well you understand the concepts in this lab: 
+[ ] I understand very little about this lab.
+[ ] I am about 50/50 on this lab; I get parts of it but not the whole picture.
+[ ] I pretty much get it.
+[ ] I'm solid. Totally got it.
+'''
